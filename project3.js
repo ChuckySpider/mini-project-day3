8d7f1737e3eb9cc5e2c39ff931415384ae17495b
ChuckySpider <chucky96911@gmail.com>
2026-02-14 10:56:00 -0600

    red and blue paint

--- project3.js
+++ project3.js
@@ -1,16 +1,20 @@
 function setup() {
-  
   createCanvas(400, 400);
 }
 
 function draw() {
   
+  if (mouseIsPressed === true) {
+    fill("blue");
+  } else {
+    fill("red");
+  }
   noStroke(0);
-  fill('red')
   circle(mouseX, mouseY, 30)
+  
 }
 
 function mousePressed(){
+  background('black') 
   
-  background(50);
 }
